ec815e7f571ed668ba128348630c224292d28f56
GNataliya <80199130+GNataliya@users.noreply.github.com>
2022-01-14 01:21:13 +0200

    cart

--- src/actions/basket.js
+++ src/actions/basket.js
@@ -1,16 +1,5 @@
 import { ADD_TO_BASKET, REMOVE_FROM_BASKET, INCREASE_QUANTITY, DECREASE_QUANTITY } from './type';
 
-// const item = [];
-    // result.data.forEach((i) => {
-    //     const cart = {
-    //         status: 'check',
-    //         id: i.id,
-    //         title: i.title,
-    //         price: i.price
-    //     }
-    //     item.push(cart)
-    // })
-
 export const addToBasket = (payload) => {
 
     const item = {
